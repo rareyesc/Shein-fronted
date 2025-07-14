9c1cd9dfb0eb249337471251ca3dd846f99d2261
Ruben Andres Reyes Cuellar <rareyesc19960629@gmail.com>
2025-07-14 13:35:55 -0500

    Arreglando bugs de vite y xlsx

--- dist-node/vite.config.js
+++ dist-node/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
     resolve: {
@@ -12,5 +11,18 @@ export default defineConfig({
     },
     server: {
         port: 8079
+    },
+    build: {
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        const dirs = id.toString().split('node_modules/')[1].split('/');
+                        return dirs[0];
+                    }
+                }
+            }
+        }
     }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -13,5 +12,18 @@ export default defineConfig({
   },
   server: {
     port: 8079
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const dirs = id.toString().split('node_modules/')[1].split('/')
+            return dirs[0]
+          }
+        }
+      }
+    }
   }
 })
